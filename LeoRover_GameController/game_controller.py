32b63483e0bccd65878a029ee4e7aaa7cb647a15
Dennis Buchberger <75340117+buchbergerd@users.noreply.github.com>
2023-07-05 09:34:20 +0000

    Initial. Read the state of a game controller and publish cmd_vel messages.

---
+++ LeoRover_GameController/game_controller.py
@@ -0,0 +1,134 @@
+from __future__ import annotations
+import rclpy
+from rclpy.node import Node
+from geometry_msgs.msg import Twist
+
+from inputs import get_gamepad
+
+import math
+import threading
+import time
+
+
+class XboxController(object):
+    MAX_TRIG_VAL = math.pow(2, 8)
+    MAX_JOY_VAL = math.pow(2, 15)
+
+    def __init__(self):
+
+        self.LeftJoystickY = 0
+        self.LeftJoystickX = 0
+        self.RightJoystickY = 0
+        self.RightJoystickX = 0
+        self.LeftTrigger = 0
+        self.RightTrigger = 0
+        self.LeftBumper = 0
+        self.RightBumper = 0
+        self.A = 0
+        self.X = 0
+        self.Y = 0
+        self.B = 0
+        self.LeftThumb = 0
+        self.RightThumb = 0
+        self.Back = 0
+        self.Start = 0
+        self.LeftDPad = 0
+        self.RightDPad = 0
+        self.UpDPad = 0
+        self.DownDPad = 0
+
+        self._monitor_thread = threading.Thread(target=self._monitor_controller, args=())
+        self._monitor_thread.daemon = True
+        self._monitor_thread.start()
+
+        self.publisher = None
+
+    def connectPublisher(self, publisher :VelocityPublisher = None):
+        self.publisher = publisher
+
+    def _monitor_controller(self):
+        while True:
+            events = get_gamepad()
+            for event in events:
+                if event.code == 'ABS_Y':
+                    # print(f"{event.state} / {XboxController.MAX_JOY_VAL}")
+                    # event.state = 0
+                    self.LeftJoystickY = round(event.state / XboxController.MAX_JOY_VAL, 3) # normalize between -1 and 1
+                elif event.code == 'ABS_X':
+                    self.LeftJoystickX = round(event.state / XboxController.MAX_JOY_VAL, 3) # normalize between -1 and 1
+                elif event.code == 'ABS_RY':
+                    self.RightJoystickY = round(event.state / XboxController.MAX_JOY_VAL, 3) # normalize between -1 and 1
+                elif event.code == 'ABS_RX':
+                    self.RightJoystickX = round(event.state / XboxController.MAX_JOY_VAL, 3) # normalize between -1 and 1
+                elif event.code == 'ABS_Z':
+                    self.LeftTrigger = event.state / XboxController.MAX_TRIG_VAL # normalize between 0 and 1
+                elif event.code == 'ABS_RZ':
+                    self.RightTrigger = event.state / XboxController.MAX_TRIG_VAL # normalize between 0 and 1
+                elif event.code == 'BTN_TL':
+                    self.LeftBumper = event.state
+                elif event.code == 'BTN_TR':
+                    self.RightBumper = event.state
+                elif event.code == 'BTN_SOUTH':
+                    self.A = event.state
+                elif event.code == 'BTN_NORTH':
+                    self.Y = event.state #previously switched with X
+                elif event.code == 'BTN_WEST':
+                    self.X = event.state #previously switched with Y
+                elif event.code == 'BTN_EAST':
+                    self.B = event.state
+                elif event.code == 'BTN_THUMBL':
+                    self.LeftThumb = event.state
+                elif event.code == 'BTN_THUMBR':
+                    self.RightThumb = event.state
+                elif event.code == 'BTN_SELECT':
+                    self.Back = event.state
+                elif event.code == 'BTN_START':
+                    self.Start = event.state
+                elif event.code == 'BTN_TRIGGER_HAPPY1':
+                    self.LeftDPad = event.state
+                elif event.code == 'BTN_TRIGGER_HAPPY2':
+                    self.RightDPad = event.state
+                elif event.code == 'BTN_TRIGGER_HAPPY3':
+                    self.UpDPad = event.state
+                elif event.code == 'BTN_TRIGGER_HAPPY4':
+                    self.DownDPad = event.state
+            if self.publisher is not None:
+                self.publisher.publish()
+
+
+class VelocityPublisher(Node):
+
+    def __init__(self, joystick :XboxController):
+        super().__init__('velocity_publisher')
+        self.publisher_ = self.create_publisher(Twist, 'cmd_vel', 10)
+        timer_period = 0.4  # seconds
+        self.timer = self.create_timer(timer_period, self.timer_callback)
+        self.linear = 0.
+        self.angular = 0.
+        self.joystick :XboxController = joystick
+
+    def publish(self):
+        msg = Twist()
+        msg.linear.x = float(-self.joystick.LeftJoystickY * .4)
+        msg.angular.z = float(-self.joystick.LeftJoystickX * 1.)
+        self.publisher_.publish(msg)
+        self.get_logger().info(f"Publishing to cmd_vel: {msg}")
+
+    def timer_callback(self):
+        self.publish()
+
+
+def main(args=None):
+    rclpy.init()
+    joystick = XboxController()
+
+    minimal_publisher = VelocityPublisher(joystick)
+
+    rclpy.spin(minimal_publisher)
+
+    minimal_publisher.destroy_node()
+    rclpy.shutdown()
+
+
+if __name__ == '__main__':
+    main()
