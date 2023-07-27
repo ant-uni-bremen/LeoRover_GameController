09f3fc16362584731444ff1ccb6ae2e42634a01d
Dennis Buchberger <75340117+buchbergerd@users.noreply.github.com>
2023-07-27 13:11:34 +0000

    Switched from list to queue. No timer for zero delay.

--- LeoRover_GameController/game_controller.py
+++ LeoRover_GameController/game_controller.py
@@ -11,6 +11,7 @@ import threading
 import time
 import random
 from typing import List
+import queue
 
 
 # ToDo: Right loss button debouncing
@@ -151,15 +152,23 @@ class VelocityPublisher(Node):
         self.send_timer.cancel()
         self.last_linear = 0.   # for periodic publishing
         self.last_angular = 0.  # for periodic publishing
-        self.nextCommands :List[VelocityPublisher.DelayedCommand] = []
+        # self.nextCommands :List[VelocityPublisher.DelayedCommand] = []
+        self.command_queue :queue.Queue[VelocityPublisher.DelayedCommand]= queue.Queue()
         self.delay_s = 0
         self.loss_rate = 0
         
 
-    def _publish(self):
+    def _publish(self, linear_x :float|None = None, angular_z :float|None = None):
         msg = Twist()
+
+        if linear_x is not None:
+            self.last_linear = linear_x
+        if angular_z is not None:
+            self.last_angular = angular_z
+
         msg.linear.x = self.last_linear
         msg.angular.z = self.last_angular
+
         self.publisher_.publish(msg)       
         # self.get_logger().info(f"Publishing to cmd_vel: {msg}")
 
@@ -171,38 +180,47 @@ class VelocityPublisher(Node):
             return
 
         print(f"SendCommand: {linear_x, angular_z}")
-        cmd = VelocityPublisher.DelayedCommand()
-        cmd.linear = float(linear_x)
-        cmd.angular = float(angular_z)
-        cmd.timeToSend = time.time() + self.delay_s
 
-        # ToDo: Sonderfall 0
+        if self.delay_s == 0:
+            self.keep_alive_timer.reset()
+            self._publish(linear_x, angular_z)
+        else:
+            cmd = VelocityPublisher.DelayedCommand()
+            cmd.linear = float(linear_x)
+            cmd.angular = float(angular_z)
+            cmd.timeToSend = time.time() + self.delay_s
 
-        self.nextCommands.append(cmd)
+            # self.nextCommands.append(cmd)
+            self.command_queue.put_nowait(cmd)
 
-        if len(self.nextCommands) == 1:     # Restart the timer to send that only queued command
-            self.send_timer.timer_period_ns = self.delay_s * 1e9
-            self.send_timer.reset()            
+            # if len(self.nextCommands) == 1:     # Restart the timer to send that only queued command, as it is not running after emptying the queue
+            if self.command_queue.qsize() == 1:     # Restart the timer to send that only queued command, as it is not running after emptying the queue
+                self.send_timer.timer_period_ns = self.delay_s * 1e9
+                self.send_timer.reset()            
 
 
     def send_timer_callback(self):
         self.keep_alive_timer.reset()
 
         print(f"Send timer callback")
-        cmd = self.nextCommands.pop(0)
+        # cmd = self.nextCommands.pop(0)
+        cmd = self.command_queue.get_nowait()
         self.last_angular = cmd.angular
         self.last_linear = cmd.linear
         self._publish()
         
-        if len(self.nextCommands) == 0:
+        # if len(self.nextCommands) == 0:
+        if self.command_queue.empty():
             self.send_timer.cancel()
         else:
-            self.send_timer.timer_period_ns = (self.nextCommands[0].timeToSend - cmd.timeToSend) * 1e9
+            # self.send_timer.timer_period_ns = (self.nextCommands[0].timeToSend - cmd.timeToSend) * 1e9
+            self.send_timer.timer_period_ns = (self.command_queue.queue[0].timeToSend - cmd.timeToSend) * 1e9
             self.send_timer.reset()
 
 
     def timer_callback(self):
         self._publish()
+
 
 
 def main(args=None):
